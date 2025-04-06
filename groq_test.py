6b2028ce04cb2f6e21808419f3a68e4e56328992
Oliver Flood <oliveroldrich@gmail.com>
2025-04-06 02:05:16 -0700

    Add basic google api call test

--- groq_test.py
+++ groq_test.py
@@ -1,16 +1,34 @@
 import os
 from groq import Groq
 from dotenv import load_dotenv
+from google.cloud import aiplatform
+
+
+##############################
+# Groq sentence descriptions #
+##############################
 
 load_dotenv()
-
 client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
-response = client.chat.completions.create(
-    model="llama3-8b-8192",
-    messages=[
-        {"role": "user", "content": "Say hello world in a fancy way"}
-    ]
-)
+def describe_image(image_url):
+    response = client.chat.completions.create(
+        model="meta-llama/llama-4-scout-17b-16e-instruct",
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": "Describe this album cover in a sentence."},
+                    {"type": "image_url", "image_url": {"url": image_url}},
+                ],
+            }
+        ],
+        temperature=0.7,
+        max_completion_tokens=256,
+    )
 
-print(response.choices[0].message.content)
+    return response.choices[0].message.content
+
+image_url = "https://i.scdn.co/image/ab67616d0000b273bba7cfaf7c59ff0898acba1f"
+description = describe_image(image_url)
+print(description)
